4ecd384fda2a2bd23a11d7182decb96dc4680aed
Haqqi <me@haqqi.net>
2018-03-13 08:31:48 +0700

    add getConnection

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,15 @@
 
 namespace haqqi\tests\arangodb;
 
+use haqqi\arangodb\Connection;
 use yii\console\Application;
 use yii\helpers\ArrayHelper;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
+    /** @var Connection */
+    protected $arangodb;
+
     protected function mockApplication($config = [], $appClass = Application::class)
     {
         new $appClass(ArrayHelper::merge([
@@ -20,5 +24,27 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
     protected function destroyApplication()
     {
         \Yii::$app = null;
+    }
+
+    /**
+     * @since 2018-03-13 08:30:56
+     *
+     * @param bool  $reset
+     * @param array $config
+     *
+     * @return Connection|object
+     * @throws \yii\base\InvalidConfigException
+     */
+    protected function getConnection($reset = false, $config = [])
+    {
+        if (!$reset && !\is_null($this->arangodb)) {
+            return $this->arangodb;
+        }
+
+        $config = ArrayHelper::merge(require(__DIR__ . '/data/config.php'), $config);
+
+        $this->arangodb = \Yii::createObject($config);
+
+        return $this->arangodb;
     }
 }
