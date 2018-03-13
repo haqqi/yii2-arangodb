d36730ce12bccad37fe670f6b6ed127c7ff75598
Haqqi <me@haqqi.net>
2018-03-13 07:45:19 +0700

    add mockApplication

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,18 @@
 
 namespace haqqi\tests\arangodb;
 
+use yii\console\Application;
+use yii\helpers\ArrayHelper;
+
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
-
+    protected function mockApplication($config = [], $appClass = Application::class)
+    {
+        new $appClass(ArrayHelper::merge([
+            'id'          => 'yii2-arangodb-test',
+            'basePath'    => __DIR__,
+            'vendorPath'  => dirname(__DIR__),
+            'runtimePath' => __DIR__ . '/runtime'
+        ], $config));
+    }
 }
