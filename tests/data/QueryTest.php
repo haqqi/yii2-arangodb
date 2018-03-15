13e0d1d189d4dccd71b8566a27162323cadd2232
L Shaf <shafry2008@gmail.com>
2018-03-15 15:38:33 +0700

    Query Test
    adding more where and TODO for @haqqi

--- tests/data/QueryTest.php
+++ tests/data/QueryTest.php
@@ -17,12 +17,15 @@ class QueryTest extends TestCase
         $query = new Query();
         $query->select(["_id", "_key"])
             ->from("post")
+            ->where(["_id" => 'asd'])
             ->andWhere(["_id" => '1112'])
-            ->andWhere(['OR', ['_id' => '12222'], ['!=', "_id", "44444"]]);
+            ->andWhere(['OR', ['_id' => '12222'], ['!=', "_id", "44444"]])
+            ->andWhere(['LIKE', 'regex', 'regex']);
 
         $build = new QueryBuilder($query);
         echo $build->build();
         
+        // @TODO mas haq, tolong kira" ini outputnya gimana? untuk penyesuaian
         $raw = "FOR `post` IN `post` RETURN { _id: `post`.`_id`, _key: `post`.`_key`}";
         $this->assertEquals($raw, $build->build());
     }
